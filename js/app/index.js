e9aa59b992c6f0fc33b521e72f1eeb429405497c
Philippe Barbosa <contact@philippebarbosa.com>
2019-04-05 14:54:51 +0200

    Better arrows for slick slider

--- js/app/index.js
+++ js/app/index.js
@@ -26,7 +26,9 @@ var app = {
             // fade: true,
             // autoplay:true,
             // autoplaySpeed: 5000,
-            arrow: true,
+            arrows: true,
+            prevArrow: '<button type="button" class="slick-prev slider__prev"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 240.823 240.823"><path d="M57.633 129.007L165.93 237.268c4.752 4.74 12.451 4.74 17.215 0 4.752-4.74 4.752-12.439 0-17.179l-99.707-99.671 99.695-99.671c4.752-4.74 4.752-12.439 0-17.191-4.752-4.74-12.463-4.74-17.215 0L57.621 111.816c-4.679 4.691-4.679 12.511.012 17.191z"/></svg></button>',
+            nextArrow: '<button type="button" class="slick-next slider__next"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 240.823 240.823"><path d="M183.189 111.816L74.892 3.555c-4.752-4.74-12.451-4.74-17.215 0-4.752 4.74-4.752 12.439 0 17.179l99.707 99.671-99.695 99.671c-4.752 4.74-4.752 12.439 0 17.191 4.752 4.74 12.463 4.74 17.215 0l108.297-108.261c4.68-4.691 4.68-12.511-.012-17.19z"/></svg></button>',
             infinite: true,
             slidesToShow: 1,
             slidesToScroll: 1
